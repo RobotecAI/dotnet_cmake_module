9a82553d76660153c8fc7ce90b774aec3780e9ce
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2022-06-28 10:32:18 +0200

    Renamed MONO to mono (surpress warnings)

--- cmake/Modules/dotnet/FindMono.cmake
+++ cmake/Modules/dotnet/FindMono.cmake
@@ -7,29 +7,29 @@
 # Find the xbuild tool and mono interpreter
 #
 # MONO_FOUND             System has Mono dev files, as well as mono, mcs, gmcs and gacutil if not MONO_ONLY_LIBRARIES_REQUIRED
-# MONO_EXECUTABLE        Where to find 'mono'
+# Mono_EXECUTABLE        Where to find 'mono'
 # XBUILD_EXECUTABLE      Where to find 'xbuild'
-# MONO_VERSION           The version number of the Mono interpreter
+# Mono_VERSION           The version number of the Mono interpreter
 
-set(MONO_ROOT "" CACHE PATH "Set the location of the Mono root directory")
+set(Mono_ROOT "" CACHE PATH "Set the location of the Mono root directory")
 
-if(MONO_ROOT AND EXISTS "${MONO_ROOT}")
-    find_program(MONO_EXECUTABLE mono
-            PATHS ${MONO_ROOT} ${MONO_ROOT}/lib/mono/1.0
+if(Mono_ROOT AND EXISTS "${Mono_ROOT}")
+    find_program(Mono_EXECUTABLE mono
+            PATHS ${Mono_ROOT} ${Mono_ROOT}/lib/mono/1.0
             PATH_SUFFIXES bin
             NO_DEFAULT_PATH)
     find_program(XBUILD_EXECUTABLE xbuild
-            PATHS ${MONO_ROOT} ${MONO_ROOT}/lib/mono/1.0
+            PATHS ${Mono_ROOT} ${Mono_ROOT}/lib/mono/1.0
             PATH_SUFFIXES bin
             NO_DEFAULT_PATH)
 endif()
 
-find_program(MONO_EXECUTABLE mono)
+find_program(Mono_EXECUTABLE mono)
 find_program(XBUILD_EXECUTABLE xbuild)
 
 if(WIN32)
-    set(MONO_CLR "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Novell\\Mono;DefaultCLR]")
-    get_filename_component(csharp_mono_bin_hints "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Novell\\Mono\\${MONO_CLR};SdkInstallRoot]/bin" ABSOLUTE)
+    set(Mono_CLR "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Novell\\Mono;DefaultCLR]")
+    get_filename_component(csharp_mono_bin_hints "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Novell\\Mono\\${Mono_CLR};SdkInstallRoot]/bin" ABSOLUTE)
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     set(csharp_mono_bin_hints
             "/Library/Frameworks/Mono.framework/Commands"
@@ -45,7 +45,7 @@ else()
             "/opt/novell/mono/bin")
 endif()
 
-find_program(MONO_EXECUTABLE mono mono.exe
+find_program(Mono_EXECUTABLE mono mono.exe
         HINTS ${csharp_mono_bin_hints}
         PATH_SUFFIXES bin
         NO_DEFAULT_PATH
@@ -57,16 +57,16 @@ find_program(XBUILD_EXECUTABLE xbuild xbuild.exe
         NO_DEFAULT_PATH
 )
 
-if(EXISTS "${MONO_EXECUTABLE}")
+if(EXISTS "${Mono_EXECUTABLE}")
     execute_process(
-      COMMAND ${MONO_EXECUTABLE} -V
+      COMMAND ${Mono_EXECUTABLE} -V
       OUTPUT_VARIABLE csharp_mono_version_string
     )
 string(REGEX MATCH "([0-9]*)([.])([0-9]*)([.]*)([0-9]*)" csharp_mono_version_temp "${csharp_mono_version_string}")
-    set(MONO_VERSION ${csharp_mono_version_temp} CACHE STRING "C# Mono interpreter version")
+    set(Mono_VERSION ${csharp_mono_version_temp} CACHE STRING "C# Mono interpreter version")
 endif()
 
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MONO DEFAULT_MSG XBUILD_EXECUTABLE MONO_EXECUTABLE)
-mark_as_advanced(MONO_EXECUTABLE XBUILD_EXECUTABLE MONO_VERSION)
+find_package_handle_standard_args(Mono DEFAULT_MSG XBUILD_EXECUTABLE Mono_EXECUTABLE)
+mark_as_advanced(Mono_EXECUTABLE XBUILD_EXECUTABLE Mono_VERSION)
